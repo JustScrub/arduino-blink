6bbcc412637fa04e2daebc7c8d3b04afc104356f
Jakub Mladý <91391319+JustScrub@users.noreply.github.com>
2024-04-25 11:10:10 +0200

    Semaphore blink

--- blink.ino
+++ blink.ino
@@ -4,21 +4,47 @@
  
   This example code is in the public domain.
  */
- 
+
+#define SEM_LED 1
+
+#if SEM_LED
+  int green = 10;
+  int yellow = 11;
+  int red = 12;
+#else
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led = 13;
+  int led = 13;
+#endif
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+  #if SEM_LED
+    pinMode(led, OUTPUT);
+    pinMode(led, OUTPUT);
+    pinMode(led, OUTPUT);
+  #else
+    pinMode(led, OUTPUT);
+  #endif
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+  #if SEM_LED
+   digitalWrite(green,HIGH);
+  delay(3000);
+  digitalWrite(green,LOW);
+   digitalWrite(yellow,HIGH);
+  delay(1000);
+  digitalWrite(yellow,LOW);
+   digitalWrite(red,HIGH);
+  delay(5000);
+  digitalWrite(red,LOW);
+  #else
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(3000);               // long wait
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);               // short wait
+  #endif
 }
